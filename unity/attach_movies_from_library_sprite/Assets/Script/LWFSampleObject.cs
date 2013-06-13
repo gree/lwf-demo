d5b71bb1e870b0c5416a66d58d89cc8f03bfa7d0
Hiroyuki Haga <hiroyuki.haga@gree.net>
2013-06-13 18:43:08 +0900

    enterFrame callback is added for demonstrates solution at https://github.com/gree/lwf/issues/18

--- unity/attach_movies_from_library_sprite/Assets/Script/LWFSampleObject.cs
+++ unity/attach_movies_from_library_sprite/Assets/Script/LWFSampleObject.cs
@@ -6,6 +6,8 @@ public class LWFSampleObject : LWFObject {
 	
 	public string linkage_name = "lightening";
 	
+	private string message ="";
+	
 	void Start()
 	{
 		// directory name that contains lwf(.bytes) and png textures
@@ -27,24 +29,43 @@ public class LWFSampleObject : LWFObject {
 		if ( GUILayout.Button( "Attach movie from library sprite" )){
 			if (lwf !=null){
 				// The linkage named "gree" is instantiated and attached to root movie.
+				
+				string instance_name = linkage_name + attach_count.ToString();
 				LWF.Movie attached_movie = 
 					lwf.rootMovie.AttachMovie(
 						linkage_name, // Symbol's linkage name in library
-						linkage_name + attach_count.ToString() // Attached movie name.
+						instance_name,  // Attached movie name.
 						// NOTE: Atttached movie name should unique. 
 						// If duplicated name is specified , new instance is not created and existing instance is returned.
+						enterFrame: enterFrameCallback // Callback that called every frame.
 					);
+				
 				
 				if ( attached_movie != null ){
 					
 					// Randomize position
 					attached_movie.MoveTo( Random.Range(0, 500), Random.Range(0, 500)); 
 					
+				
+					
 					// Increament count for unique attached movie name.
 					attach_count ++;	
 				}
 			}
 		}
+		
+		
+		GUILayout.Box( message ) ;
+	}
+	
+	void enterFrameCallback( LWF.Movie movie )
+	{
+		if (movie.currentFrame == movie.totalFrames
+			|| !movie.playing) {
+			
+			message += movie.GetFullName() + " is done \n";
+			lwf.rootMovie.DetachMovie( movie );
+    	}
 	}
 
 }
