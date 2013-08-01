e1ff8c3e1fec24a4f3529089ab9c91ae7d635211
Kazuki Sakamoto <sakamoto@splhack.org>
2013-08-01 10:38:42 -0700

    BulletML + LWF

---
+++ html5/bulletml/bulletml.js
@@ -0,0 +1,486 @@
+function log(msg) {
+	console.log(msg);
+}
+
+function getChild(n, t) {
+    for (var i = 0; i < n.childNodes.length; i++) {
+        var c = n.childNodes[i];
+        if (c.tagName == t) {
+            return c;
+        }
+    }
+    return null;
+}
+
+function BulletMLError(msg) {
+    this.name = 'BulletMLError';
+    this.message = msg;
+    this.description = this.name + ': message ' + this.message;
+}
+BulletMLError.prototype = new Error;
+
+function BulletMLState(bulletml, nodes, params) {
+    this.bulletml = bulletml;
+    this.nodes = nodes;
+    this.params = params;
+}
+
+function BulletMLRunnerImpl(state, runner) {
+    this.state = state;
+    this.isEnd = false;
+    this.actIndex = 0;
+    this.act = this.state.nodes[0];
+    this.actTurn = -1;
+    this.runner = runner;
+    this.repeatStack = [];
+    this.refStack = [];
+    this.bulletDir = null;
+    this.bulletSpd = null;
+    this.prevDir = 0;
+    this.prevSpd = 0;
+
+    this.dirChangeVal = 0;
+    this.dirChangeCnt = 0;
+    this.spdChangeVal = 0;
+    this.spdChangeCnt = 0;
+    this.accelXVal = 0;
+    this.accelXCnt = 0;
+    this.accelYVal = 0;
+    this.accelYCnt = 0;
+}
+
+BulletMLRunnerImpl.prototype.getNumberContent = function(n) {
+    var e = n.textContent;
+    e = e.replace(/\$rank/g, this.runner.obj.getRank());
+    for (var i = 0; i < this.state.params.length; i++) {
+        var reg = new RegExp('\\$' + (i + 1), 'g');
+        var p = '(' + this.state.params[i] + ')';
+        e = e.replace(reg, p);
+    }
+    while (e.match(/\$rand/)) {
+        e = e.replace(/\$rand/, Math.random());
+    }
+    if (e.match(/[^-()+*\/0-9\. \n\t]/)) {
+        throw new BulletMLError('Invalid expr: ' + n.textContent +
+                                ' (=> ' + e + ')');
+    }
+    return eval(e);
+}
+
+BulletMLRunnerImpl.prototype.isTurnEnd = function() {
+    return this.isEnd || this.actTurn > this.endTurn;
+};
+
+BulletMLRunnerImpl.prototype.getDirection = function(n, type) {
+    var d = this.getNumberContent(n);
+    if (!type) type = n.getAttribute('type');
+    if (!type || type == 'aim') {
+        d += this.runner.obj.getAimDirection();
+    }
+    else if (type == 'absolute') {
+        if (this.runner.isHorizontal) {
+            d -= 90;
+        }
+    }
+    else if (type == 'sequence') {
+        d += this.prevDir;
+    }
+    else if (type == 'relative') {
+        d += this.runner.obj.getBulletDirection();
+    }
+    return d;
+};
+
+BulletMLRunnerImpl.prototype.setDirection = function(n) {
+    var n = getChild(n, 'direction');
+    if (n) {
+        this.bulletDir = this.getDirection(n);
+    }
+};
+
+BulletMLRunnerImpl.prototype.getSpeed = function(n, type) {
+    var v = this.getNumberContent(n);
+    if (!type) type = n.getAttribute('type');
+    if (!type || type == 'absolute') {
+    }
+    else if (type == 'sequence') {
+        v += this.prevSpd;
+    }
+    else if (type == 'relative') {
+        v += this.runner.obj.getBulletSpeed();
+    }
+    return v;
+};
+
+BulletMLRunnerImpl.prototype.setSpeed = function(n) {
+    var n = getChild(n, 'speed');
+    if (n) {
+        this.bulletSpd = this.getSpeed(n);
+        //log('speed: '+ v);
+    }
+};
+
+BulletMLRunnerImpl.prototype.getNextNode = function(n) {
+    if (n.parentNode.tagName == 'bulletml' ||
+        n == this.state.nodes[this.actIndex-1]) {
+        var top = this.refStack[this.refStack.length - 1];
+        if (top) {
+            n = top[0];
+            this.state.params = top[1];
+            this.refStack.pop();
+        }
+        else {
+            ++this.actIndex;
+            if (n == this.state.nodes[this.actIndex]) {
+                return this.state.nodes[this.actIndex];
+            }
+            else {
+                this.isEnd = true;
+                log('bulletml finished');
+                return null;
+            }
+        }
+    }
+    var nn = n.nextSibling;
+    if (!nn) {
+        n = n.parentNode;
+        if (n.tagName == 'repeat') {
+            var top = this.repeatStack[this.repeatStack.length - 1];
+            if (top) {
+                top[0]--;
+                if (top[0] > 0) {
+                    nn = top[1];
+                    return nn;
+                }
+                else {
+                    this.repeatStack.pop();
+                }
+            }
+            else {
+                throw new BulletMLRunnerImpl(
+                    'internal error: repeatStack is empty');
+            }
+        }
+        nn = this.getNextNode(n);
+    }
+    return nn;
+};
+
+BulletMLRunnerImpl.prototype.calcAccel = function(xy, n, term) {
+    var val = this.getNumberContent(n);
+
+    var type = n.getAttribute('type');
+    if (type == 'sequence') {
+    }
+    else if (type == 'relative') {
+        val += this.runner.obj['getBulletSpeed' + xy]();
+        val /= term;
+    }
+    else {
+        val /= term;
+    }
+
+    this['accel' + xy + 'Val'] = val;
+    this['accel' + xy + 'Cnt'] = term;
+};
+
+BulletMLRunnerImpl.prototype.run = function() {
+    if (this.dirChangeCnt > 0) {
+        this.dirChangeCnt--;
+        var d = this.runner.obj.getBulletDirection() + this.dirChangeVal;
+        this.runner.obj.doChangeDirection(d);
+    }
+    if (this.spdChangeCnt > 0) {
+        this.spdChangeCnt--;
+        var v = this.runner.obj.getBulletSpeed() + this.spdChangeVal;
+        this.runner.obj.doChangeSpeed(v);
+        //log(v);
+    }
+    if (this.accelXCnt > 0) {
+        this.accelXCnt--;
+        var v = this.runner.obj.getBulletSpeedX() + this.accelXVal;
+        this.runner.obj.doAccelX(v);
+        //log(v);
+    }
+    if (this.accelYCnt > 0) {
+        this.accelYCnt--;
+        var v = this.runner.obj.getBulletSpeedY() + this.accelYVal;
+        this.runner.obj.doAccelY(v);
+        //log(v);
+    }
+
+    if (this.isEnd) return;
+
+    this.endTurn = this.runner.obj.getTurn();
+
+    if (this.actTurn == -1) {
+        this.actTurn = this.runner.obj.getTurn();
+    }
+
+    var cnt = 0;
+    while (this.act && !this.isTurnEnd()) {
+        var n = this.act;
+        var t = n.tagName;
+        //if (t) log('cmd@' + timer.tick + ': ' + t);
+        if (!t || t == 'speed' || t == 'direction') {
+            this.act = n.nextSibling;
+        }
+        else if (t == 'action') {
+            this.act = n.firstChild;
+        }
+        else if (t == 'fire') {
+            this.bulletSpd = null;
+            this.bulletDir = null;
+            this.setDirection(n);
+            this.setSpeed(n);
+            this.act = n.firstChild;
+        }
+        else if (t == 'bullet') {
+            this.setDirection(n);
+            this.setSpeed(n);
+
+            if (!this.bulletSpd) {
+                this.bulletSpd = this.runner.obj.getDefaultSpeed();
+            }
+            if (!this.bulletDir) {
+                this.bulletDir = this.runner.obj.getAimDirection();
+            }
+
+            var acts = [];
+            var children = n.childNodes;
+            for (var i = 0; i < children.length; i++) {
+                var c = children[i];
+                var t = c.tagName;
+                if (t == 'action' || t == 'actionRef') {
+                    acts.push(c);
+                }
+            }
+
+            this.prevDir = this.bulletDir;
+            this.prevSpd = this.bulletSpd;
+            if (acts.length > 0) {
+                var state = new BulletMLState(this.runner.bulletml,
+                                              acts,
+                                              this.state.params);
+                this.runner.obj.createBullet(state,
+                                             this.bulletDir, this.bulletSpd);
+            }
+            else {
+                this.runner.obj.createSimpleBullet(this.bulletDir,
+                                                   this.bulletSpd);
+            }
+            this.act = null;
+        }
+        else if (t.match(/^([a-z]+)Ref$/)) {
+            var type = RegExp.$1;
+            var map = this.state.bulletml[type];
+            if (!map) {
+                throw new BulletMLError('Undefined ref tag: ' + t);
+            }
+
+            this.refStack.push([n, this.state.params]);
+
+            var params = [];
+            var children = n.childNodes;
+            for (var i = 0; i < children.length; i++) {
+                var c = children[i];
+                if (c.tagName == 'param') {
+                    params.push(this.getNumberContent(c));
+                    log('param: '+params.join(','));
+                }
+            }
+            this.state.params = params;
+
+            var label = n.getAttribute('label');
+            n = map[label];
+            if (!n) {
+                throw new BulletMLError(
+                    'Undefined ' + type + ' label: ' + label);
+            }
+            this.act = n;
+        }
+        else if (t == 'wait') {
+            this.actTurn += this.getNumberContent(n);
+            this.act = null;
+        }
+        else if (t == 'changeSpeed') {
+            var termNode = getChild(n, 'term');
+            if (!termNode) {
+                throw new BulletMLError('changeSpeed must have term');
+            }
+            var term = this.getNumberContent(termNode);
+            if (term < 1) term = 1;
+
+            var spdNode = getChild(n, 'speed');
+            if (!spdNode) {
+                throw new BulletMLError('changeSpeed must have speed');
+            }
+            var type = spdNode.getAttribute('type');
+
+            var spd;
+            if (type == 'sequence') {
+                spd = this.getNumberContent(spdNode);
+            }
+            else {
+                var speed = this.getSpeed(spdNode, type);
+                var spdFirst = this.runner.obj.getBulletSpeed();
+                spd = (speed - spdFirst) / term;
+                log('changeSpd2: ' + speed + ',' + spdFirst);
+            }
+
+            this.spdChangeCnt = term;
+            this.spdChangeVal = spd;
+            log('changeSpd: ' + this.spdChangeCnt + ',' + this.spdChangeVal);
+
+            this.act = null;
+        }
+        else if (t == 'changeDirection') {
+            var termNode = getChild(n, 'term');
+            if (!termNode) {
+                throw new BulletMLError('changeDirection must have term');
+            }
+            var term = this.getNumberContent(termNode);
+            if (term < 1) term = 1;
+
+            var dirNode = getChild(n, 'direction');
+            if (!dirNode) {
+                throw new BulletMLError('changeDirection must have direction');
+            }
+            var type = dirNode.getAttribute('type');
+
+            var dir;
+            if (type == 'sequence') {
+                dir = this.getNumberContent(dirNode);
+            }
+            else {
+                var direction = this.getDirection(dirNode, type);
+                var dirFirst = this.runner.obj.getBulletDirection();
+
+                // I didn't see this logic...
+                var dirSpace;
+                var dirSpace1 = direction - dirFirst;
+                var dirSpace2;
+                if (dirSpace1 > 0) dirSpace2 = dirSpace1 - 360;
+                else dirSpace2 = dirSpace1 + 360;
+                if (Math.abs(dirSpace1) < Math.abs(dirSpace2)) {
+                    dirSpace = dirSpace1;
+                }
+                else {
+                    dirSpace = dirSpace2;
+                }
+
+                dir = dirSpace / term;
+            }
+
+            this.dirChangeCnt = term;
+            this.dirChangeVal = dir;
+            log('changeDir: ' + this.dirChangeCnt + ',' + this.dirChangeVal);
+
+            this.act = null;
+        }
+        else if (t == 'accel') {
+            var termNode = getChild(n, 'term');
+            if (!termNode) {
+                throw new BulletMLError('accel must have term');
+            }
+            var term = this.getNumberContent(termNode);
+            if (term < 1) term = 1;
+
+            var hNode = getChild(n, 'horizontal');
+            var vNode = getChild(n, 'vertical');
+
+            if (this.runner.isHorizontal) {
+                if (vNode) this.calcAccel('X', vNode, term);
+                if (hNode) this.calcAccel('Y', hNode, term);
+            }
+            else {
+                if (hNode) this.calcAccel('X', hNode, term);
+                if (vNode) this.calcAccel('Y', vNode, term);
+            }
+
+            this.act = null;
+        }
+        else if (t == 'repeat') {
+            var times = getChild(n, 'times');
+            if (!times) {
+                throw new BulletMLError('repeat must have times');
+            }
+            var t = this.getNumberContent(times);
+
+            var actionNode = getChild(n, 'action');
+            if (!actionNode) {
+                actionNode = getChild(n, 'actionRef');
+                if (!actionNode) {
+                    throw new BulletMLError(
+                        'repeat must have action or actionRef');
+                }
+            }
+
+            this.repeatStack.push([t, actionNode]);
+            this.act = actionNode;
+        }
+        else if (t == 'vanish') {
+            this.act = null;
+            this.isEnd = true;
+            this.runner.obj.doVanish();
+            break;
+        }
+
+        if (!this.act) {
+            this.act = this.getNextNode(n);
+        }
+
+        cnt++;
+        if (cnt > 10000) {
+            throw new BulletMLError('10000 commands were executed in this frame');
+        }
+    }
+
+    //this.isEnd = true;
+};
+
+function BulletMLRunner(bulletml, state) {
+    this.bulletml = bulletml;
+
+    this.isHorizontal = false;
+    if (bulletml.getAttribute('type') == 'horizontal') {
+        this.isHorizontal = true;
+    }
+
+    if (state) {
+        this.impls = [new BulletMLRunnerImpl(state, this)];
+    }
+    else {
+        var types = ['action', 'fire', 'bullet'];
+        for (var j = 0; j < 3; j++) {
+            var t = types[j];
+            bulletml[t] = {};
+            var nodes = bulletml.getElementsByTagName(t);
+            for (var i = 0; i < nodes.length; i++) {
+                var n = nodes[i];
+                var label = n.getAttribute('label');
+                if (label) {
+                    bulletml[t][label] = n;
+                }
+            }
+        }
+
+        this.impls = new Array();
+        for (var label in bulletml.action) {
+            if (label.match(/^top/)) {
+                var state = new BulletMLState(bulletml,
+                                              [bulletml.action[label]],
+                                              []);
+                this.impls.push(new BulletMLRunnerImpl(state, this));
+            }
+        }
+
+        log('Actions: ' + this.impls.length);
+    }
+}
+
+BulletMLRunner.prototype.run = function() {
+    for (var i = 0; i < this.impls.length; i++) {
+        this.impls[i].run();
+    }
+};
---
+++ html5/bulletml/game.coffee
@@ -0,0 +1,387 @@
+#
+# Copyright (C) 2013 GREE, Inc.
+#
+# This software is provided 'as-is', without any express or implied
+# warranty.  In no event will the authors be held liable for any damages
+# arising from the use of this software.
+#
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely, subject to the following restrictions:
+#
+# 1. The origin of this software must not be misrepresented; you must not
+#    claim that you wrote the original software. If you use this software
+#    in a product, an acknowledgment in the product documentation would be
+#    appreciated but is not required.
+# 2. Altered source versions must be plainly marked as such, and must not be
+#    misrepresented as being the original software.
+# 3. This notice may not be removed or altered from any source distribution.
+#
+
+unless window.requestAnimationFrame?
+  for vendor in ['webkit', 'moz']
+    window.requestAnimationFrame = window[vendor+'RequestAnimationFrame']
+    break if window.requestAnimationFrame?
+unless window.requestAnimationFrame?
+  lastTime = 0
+  window.requestAnimationFrame = (callback, element) ->
+    currTime = new Date().getTime()
+    lastTime = currTime if lastTime is 0
+    timeToCall = Math.max(0, 16 - (currTime - lastTime))
+    timeoutCallback = ->
+      callback(currTime + timeToCall)
+    id = window.setTimeout(timeoutCallback, timeToCall)
+    lastTime = currTime + timeToCall
+    return id
+
+game = null
+keyState = []
+timer = {}
+
+class Obj
+  constructor:(@x, @y, @d, @v, name) ->
+    @alive = true
+    @bitmap = null
+    @attachBitmap(name) if name
+
+  attachBitmap:(name) ->
+    @bitmap = game.attachBitmap(name)
+    @bitmap.regX = @bitmap.width / 2
+    @bitmap.regY = @bitmap.height / 2
+    return
+
+  move: ->
+    return
+
+  draw: ->
+    if @bitmap
+      @bitmap.x = @x
+      @bitmap.y = @y
+    return
+
+  ensureBounded: ->
+    if @x < 4
+      @x = 4
+    else if @x > window.game.width - 4
+      @x = game.width - 4
+    if @y < 4
+      @y = 4
+    else if @y > window.game.height - 4
+      @y = game.height - 4
+    return
+
+  checkBounded: ->
+    if @x < 4 or @x > window.game.width - 4 or
+        @y < 4 or @y > window.game.height - 4
+      @doVanish()
+    return
+
+  doVanish: ->
+    @alive = false
+    game.detachBitmap(@bitmap)
+    @bitmap = null
+    return
+
+class Shot extends Obj
+  constructor:(x, y, d, v, name = 'shot.png') ->
+    super(x, y, d, v, name)
+
+  move: ->
+    r = Math.PI * @d / 180
+    @x += Math.sin(r) * @v
+    @y -= Math.cos(r) * @v
+    @checkBounded()
+    return
+
+class Player extends Obj
+  constructor: ->
+    super(game.width / 2, 350, 0, 0, 'shot.png')
+
+  move: ->
+    v = if keyState[90] then 1 else 2
+    d = -1
+    if keyState[38]
+      if keyState[39]
+        d = 45
+      else if keyState[37]
+        d = -45
+      else
+        d = 0
+    else if keyState[40]
+      if keyState[39]
+        d = 135
+      else if keyState[37]
+        d = -135
+      else
+        d = 180
+    else if keyState[39]
+      d = 90
+    else if keyState[37]
+      d = -90
+
+    if d isnt -1
+      r = Math.PI * d / 180
+      @x += Math.sin(r) * v
+      @y -= Math.cos(r) * v
+      @ensureBounded()
+    return
+
+class Enemy extends Obj
+  constructor:(@runner, x, y, d, v, name, @isBoss) ->
+    super(x, y, d, v, name)
+    @runner.obj = @
+
+  move: ->
+    return unless @alive
+
+    @runner.run()
+    r = Math.PI * @d / 180
+    @x += Math.sin(r) * @v
+    @y += -Math.cos(r) * @v
+
+    if @isBoss
+      @ensureBounded()
+    else
+      @checkBounded()
+    return
+
+  getRank: ->
+    return 0
+
+  getTurn: ->
+    return timer.tick
+
+  getDefaultSpeed: ->
+    return 1
+
+  getAimDirection: ->
+    dx = game.player.x - @x
+    dy = game.player.y - @y
+    return Math.atan2(dx, -dy) * 180 / Math.PI
+
+  getBulletSpeedX: ->
+    r = Math.PI * @d / 180
+    return Math.sin(r) * @v
+
+  getBulletSpeedY: ->
+    r = Math.PI * @d / 180
+    return -Math.cos(r) * @v
+
+  getBulletDirection: ->
+    return @d
+
+  getBulletSpeed: ->
+    return @v
+
+  createSimpleBullet:(d, v) ->
+    shot = new Shot(@x, @y, d, v)
+    game.objs.push(shot)
+    return
+
+  createBullet:(state, d, v) ->
+    runner = new BulletMLRunner(state.bulletml, state)
+    hasFire = (state.nodes[0].getElementsByTagName('fire') or
+      state.nodes[0].getElementsByTagName('fireRef'))
+    color = hasFire ? 'shot.png' : 'shot.png'
+    shot = new Enemy(runner, @x, @y, d, v, color, false)
+    objs.push(shot)
+    return
+
+  doChangeDirection:(@d) ->
+    return
+
+  doChangeSpeed:(@v) ->
+    return
+
+  doAccelX:(vx) ->
+    vy = @getBulletSpeedY()
+    @v = Math.sqrt(vx * vx + vy * vy)
+    @d = Math.atan2(vx, -vy) * 180 / Math.PI
+    return
+
+  doAccelY:(vy) ->
+    vx = this.getBulletSpeedX()
+    @v = Math.sqrt(vx * vx + vy * vy)
+    @d = Math.atan2(vx, -vy) * 180 / Math.PI
+    return
+
+class Game
+  constructor:(@touchDelegate, @stage, @width, @height, @xml) ->
+    @requests = []
+
+    @stats = new Stats()
+    document.body.appendChild(@stats.domElement)
+
+    LWF.useWebGLRenderer()
+    #LWF.useCanvasRenderer()
+
+  requestLWF:(lwfName, onload) ->
+    if lwfName.match(/(.*\/)([^\/]+)/)
+      prefix = RegExp.$1
+      lwfName = RegExp.$2
+    else
+      prefix = ""
+
+    @requests.push({
+      lwf:lwfName,
+      prefix:prefix,
+      stage:@stage,
+      onload:onload,
+    })
+    return
+
+  loadLWFs:(onloadall) ->
+    LWF.ResourceCache.get().loadLWFs(@requests, onloadall)
+    @requests = []
+    return
+
+  load:(lwfName) ->
+    @requestLWF(lwfName, (lwf) => @lwf = lwf)
+    @loadLWFs((errors) => @init() unless errors?)
+    return
+
+  attachBitmap:(name) ->
+    cache = @bitmapCache[name]
+    if cache?.length > 0
+      bitmap = cache.pop()
+      bitmap.visible = true
+    else
+      bitmap = @world.attachBitmap(name, @bitmapCount++)
+    return bitmap
+
+  detachBitmap:(bitmap) ->
+    bitmap.visible = false
+    @bitmapCache[bitmap.name] ?= []
+    @bitmapCache[bitmap.name].push(bitmap)
+    return
+
+  getTime: ->
+    return Date.now() / 1000.0
+
+  inputPoint:(e) ->
+    x = e.clientX + document.body.scrollLeft +
+      document.documentElement.scrollLeft - @stage.offsetLeft
+    y = e.clientY + document.body.scrollTop +
+      document.documentElement.scrollTop - @stage.offsetTop
+    @lwf.inputPoint(x, y)
+    return
+
+  inputPress:(e) ->
+    @inputPoint(e)
+    @lwf.inputPress()
+    return
+
+  inputRelease:(e) ->
+    @inputPoint(e)
+    @lwf.inputRelease()
+    return
+
+  onmove:(e) =>
+    do (e) =>
+      @inputQueue.push(() => @inputPoint(e))
+      return
+    return
+  onpress:(e) =>
+    do (e) =>
+      @inputQueue.push(() => @inputPress(e))
+      return
+    return
+  onrelease:(e) =>
+    do (e) =>
+      @inputQueue.push(() => @inputRelease(e))
+      return
+    return
+
+  init: ->
+    @inputQueue = []
+    @lwf.rendererFactory.fitForHeight(@lwf)
+    @from = @getTime()
+
+    timer.tick = 0
+
+    @world = @lwf.rootMovie.attachEmptyMovie("world")
+    @bitmapCache = {}
+    @bitmapCount = 0
+
+    @objs = []
+
+    @player = new Player
+    @objs.push(@player)
+
+    @runner = new BulletMLRunner(@xml)
+    enemy = new Enemy(@runner, @width / 2, 100, 0, 0, 'shot.png')
+    @objs.push(enemy)
+
+    @exec()
+
+    ###
+    @touchDelegate.addEventListener("mousedown", @onpress, false)
+    @touchDelegate.addEventListener("mousemove", @onmove, false)
+    @touchDelegate.addEventListener("mouseup", @onrelease, false)
+    @touchDelegate.addEventListener("touchstart", @onpress, false)
+    @touchDelegate.addEventListener("touchmove", @onmove, false)
+    @touchDelegate.addEventListener("touchend", @onrelease, false)
+    ###
+    return
+
+  exec: ->
+    ++timer.tick
+    time = @getTime()
+    tick = time - @from
+    @from = time
+    input() for input in @inputQueue
+    @inputQueue = []
+
+    for obj in @objs
+      obj.move() if obj.alive
+
+    if timer.tick % 20 is 0
+      nobjs = []
+      nobjs.push(obj) for obj in @objs
+      @objs = nobjs
+
+    for obj in @objs
+      obj.draw() if obj.alive
+
+    @lwf.exec(tick)
+    @lwf.render()
+
+    @stats.update()
+
+    requestAnimationFrame(=> @exec())
+    return
+
+window.onload = ->
+  div = document.getElementById("touchDelegate")
+  stage = document.getElementById("stage")
+  w = stage.width
+  h = stage.height
+  stage.style.width = stage.width + "px"
+  stage.style.height = stage.height + "px"
+  stage.width *= window.devicePixelRatio
+  stage.height *= window.devicePixelRatio
+
+  src = "test.xml"
+  xhr = new XMLHttpRequest()
+  xhr.onreadystatechange = ->
+    if xhr.readyState is 4
+      unless xhr.responseXML?
+        throw new Error("#{xhr.status}:#{src}")
+      else
+        xml = xhr.responseXML.getElementsByTagName('bulletml')[0]
+        game = new Game(div, stage, w, h, xml)
+        game.load("game.lwf")
+        window.game = game
+  xhr.open('GET', src, true)
+  if (xhr.overrideMimeType)
+    xhr.overrideMimeType("application/xml")
+  xhr.send(null)
+  return
+
+document.onkeydown = (ev) ->
+  keyState[ev.keyCode] = 1 if ev?
+
+document.onkeyup = (ev) ->
+  return unless ev
+  keyState[ev.keyCode] = 0 if ev?
+
